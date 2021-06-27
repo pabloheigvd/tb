28c57121ce21b392f75bcaadced6fa8926701c58
brassens <brassens@pop-os.localdomain>
2021-06-27 16:22:32 +0200

    WIP

--- docker/iict/run-all-jobs.sh
+++ docker/iict/run-all-jobs.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 # https://stackoverflow.com/a/14505622
-cd fio-jobs/local || exit
+cd fio-jobs || exit
 for i in *.fio; do
     [ -f "$i" ] || break
     # output does not work in job mode: https://www.spinics.net/lists/fio/msg05818.html
-    fio "$i" --output "$i"-output --output-format=json
+    # fio "$i" --output "$i"-output --output-format=json
+    fio --version
 done
