bf12ccf6ae868fd9c0205d79d34a023cb62f1326
brassens <brassens@pop-os.localdomain>
2021-06-28 21:37:32 +0200

    test

--- docker/iict/run-all-jobs.sh
+++ docker/iict/run-all-jobs.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # https://stackoverflow.com/a/14505622
+cd fio-jobs || exit
 for i in *.fio; do
     [ -f "$i" ] || break
     # output does not work in job mode: https://www.spinics.net/lists/fio/msg05818.html
