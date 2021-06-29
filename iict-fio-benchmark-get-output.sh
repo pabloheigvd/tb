fcfb979d3ee68b273d0753601ff1b4507d763241
brassens <brassens@pop-os.localdomain>
2021-06-29 16:27:25 +0200

    test

--- iict-fio-benchmark-get-output.sh
+++ iict-fio-benchmark-get-output.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
 echo "Retrieving all jobs output..."
-for i in docker/iict/architecting-it-test*; do
+cd docker/iict/fio-jobs || exit
+for i in architecting-it-test*; do
     [ -f "$i" ] || break
     # shellcheck disable=SC2046
     kubectl --namespace=mercado cp \
     $(kubectl get pods --namespace=mercado -o=jsonpath='{.items[0].metadata.name}'):/fio-jobs/"$i"-output \
-    fio-jobs/iict/"$i"-output
+    ../../../fio-jobs/iict/"$i"-output
 done
 
-echo "All jobs output retrieved"
+echo "All jobs output retrieved.You can now get all the outputs:"
+ls -d "*-output"
+echo "Now sleeping tight..."
+while true; do sleep 30s; done
