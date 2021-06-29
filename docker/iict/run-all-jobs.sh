774c7c7bb7a0a2858c29059abc61f088ebb60c3e
brassens <brassens@pop-os.localdomain>
2021-06-29 14:31:00 +0200

    WIP

--- docker/iict/run-all-jobs.sh
+++ docker/iict/run-all-jobs.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 # https://stackoverflow.com/a/14505622
+echo "Will run all fio jobs shortly thereafter..."
 cd fio-jobs || exit
+echo "Jobs to be executed:"
+ls
 for i in *.fio; do
     [ -f "$i" ] || break
     # output does not work in job mode: https://www.spinics.net/lists/fio/msg05818.html
