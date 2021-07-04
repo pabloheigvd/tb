1485c37a2109093490213402ac2346c6d99f44e7
brassens <brassens@pop-os.localdomain>
2021-07-04 20:49:18 +0200

    better test

--- docker/iict/run-all-jobs.sh
+++ docker/iict/run-all-jobs.sh
@@ -6,22 +6,20 @@ cd fio-jobs || exit
 fio --version
 echo "Jobs to be executed:"
 ls
-echo "test raw-options raw-output"
-fio --randrepeat=0 --verify=0 --ioengine=libaio --direct=1 --name=read_latency \
---filename=/data/fiotest --bs=4K --iodepth=4 --size=250G --readwrite=randread \
---time_based --ramp_time=10s --runtime=30s > "test-raw-options raw-output"
 
-echo "test raw-options output option"
-fio --randrepeat=0 --verify=0 --ioengine=libaio --direct=1 --name=read_latency \
---filename=/data/fiotest --bs=4K --iodepth=4 --size=250G --readwrite=randread \
---time_based --ramp_time=10s --runtime=30s --output "test-raw-options output option"
+echo "============================================"
+echo "tes-raw-options raw-output"
+fio --randrepeat=0 --verify=0 --ioengine=libaio --direct=1 --name=read_latency --filename=/data/fiotest --bs=4K --iodepth=4 --size=250G --readwrite=randread --time_based --ramp_time=10s --runtime=30s > "test-raw-options raw-output"
 
-echo "test file options raw-output"
-fio "architecting-it-test7-sequential-read" > "test-file options raw-output"
+echo "test-raw-options json-output"
+fio --randrepeat=0 --verify=0 --ioengine=libaio --direct=1 --name=read_latency --filename=/data/fiotest --bs=4K --iodepth=4 --size=250G --readwrite=randread --time_based --ramp_time=10s --runtime=30s --output="test-raw-options output option" --output-format=json
 
-echo "test file options output option"
-fio "architecting-it-test7-sequential-read" --output "test-file options output option" \
---output-format=json
+echo "test-file-options raw-output"
+fio "architecting-it-test7-sequential-read" > "test-file-options raw-output"
+
+echo "test-file-options json-output"
+fio "architecting-it-test7-sequential-read" --output="test-file-options output option" --output-format=json
+echo "============================================"
 
 for i in *; do
     [ -f "$i" ] || break
