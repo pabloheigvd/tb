1be60f60aaf7a6086a6576160a6785c81d2b7167
brassens <brassens@pop-os.localdomain>
2021-07-04 15:26:11 +0200

    test

--- docker/iict/run-all-jobs.sh
+++ docker/iict/run-all-jobs.sh
@@ -6,11 +6,16 @@ cd fio-jobs || exit
 fio --version
 echo "Jobs to be executed:"
 ls
+echo "test"
+fio --randrepeat=0 --verify=0 --ioengine=libaio --direct=1 --name=read_latency \
+--filename=/data/fiotest --bs=4K --iodepth=4 --size=250G --readwrite=randread \
+--time_based --ramp_time=10s --runtime=30s
+
 for i in *; do
     [ -f "$i" ] || break
     echo "Executing job $i..."
     # null latency with json output
-    if [  "$i" = "architecting-it-test5-read-latency" ] || [ "$i" = "architecting-it-test5-write-latency" ]; then
+    if [  "$i" = "architecting-it-test5-read-latency" ] || [ "$i" = "architecting-it-test6-write-latency" ]; then
       fio "$i" > "$i-output"
     else
       # output does not work in job mode: https://www.spinics.net/lists/fio/msg05818.html
