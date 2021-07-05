fdd6da50aa09459fd9d05629c3cfbdd7ace85c8d
brassens <brassens@pop-os.localdomain>
2021-07-05 10:33:26 +0200

    WIP

--- iict-fio-benchmark-get-output.sh
+++ iict-fio-benchmark-get-output.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 while getopts ":h" option; do
    case $option in
@@ -16,8 +16,17 @@ if [ "$#" -eq "0" ]; then
     echo "Please, provide the configuration number of the deployment to test on IICT cluster. Exiting..."
     exit 1
 fi
-
-mkdir -p "fio-jobs-output/iict/deployment-$1-???"
+DEPLOYMENT_NAME=""
+cd deployments || exit
+for i in *; do
+  [ -f "$i" ] || break
+  if [[ $i == *"$1"* ]]; then
+    DEPLOYMENT_NAME=$(echo "$i" | cut --delimiter=. -f1)
+    echo "$DEPLOYMENT_NAME"
+  fi
+done
+cd ..
+mkdir -p "fio-jobs-output/iict/$DEPLOYMENT_NAME"
 
 echo "Retrieving all jobs output..."
 cd docker/iict/fio-jobs || exit
@@ -26,7 +35,7 @@ for i in architecting-it-test*; do
     # shellcheck disable=SC2046
     kubectl --namespace=mercado cp \
     $(kubectl get pods --namespace=mercado -o=jsonpath='{.items[0].metadata.name}'):fio-jobs/"$i"-output \
-    ../../../fio-jobs-output/iict/deployment-$1-???/"$i"-output
+    ../../../fio-jobs-output/iict/"$DEPLOYMENT_NAME"/"$i"-output
     echo "✅ $i-output"
 done
 
