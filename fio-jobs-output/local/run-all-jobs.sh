c5773d5cb3acfb8cdd6253f35e5f2719c89057dd
brassens <brassens@pop-os.localdomain>
2021-07-18 13:14:48 +0200

    Add local jobs

--- fio-jobs-output/local/run-all-jobs.sh
+++ fio-jobs-output/local/run-all-jobs.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-
-# https://stackoverflow.com/a/14505622
-cd fio-jobs/local || exit
-for i in *.fio; do
+cd jobs || exit
+fio --version
+echo "Jobs to be executed:"
+ls
+for i in *; do
     [ -f "$i" ] || break
+    echo "Executing job $i..."
     # output does not work in job mode: https://www.spinics.net/lists/fio/msg05818.html
     fio "$i" --output "$i"-output --output-format=json
 done
+
+echo "All jobs done. You can now get all the outputs:"
+ls *-output
